ae8a8194085cd3426cda71a1f80fc8b4aa5838b0
Jokcy <jokcylou@hotmail.com>
2020-08-29 21:39:08 +0800

    finish 2-5

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, defineComponent, h } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+// import App from './App.vue'
+
+const img = require('./assets/logo.png') // eslint-disable-line
+
+// createElement
+const App = defineComponent({
+  render() {
+    return h('div', { id: 'app' }, [
+      h('img', {
+        alt: 'Vue logo',
+        src: img,
+      }),
+      h(HelloWorld, {
+        msg: 'Welcome to Your Vue.js + TypeScript App',
+        age: 12,
+      }),
+    ])
+  },
+})
 
 createApp(App).mount('#app')
