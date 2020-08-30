d091ba64e857858bde8a96e209f2a3e003624310
Jokcy <jokcylou@hotmail.com>
2020-08-30 14:51:13 +0800

    finish 2-7

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,36 @@
-import { createApp, defineComponent, createVNode } from 'vue'
+import { createApp, defineComponent, h, reactive, ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import App from './App.vue'
+// import App from './App.vue'
 
 const img = require('./assets/logo.png') // eslint-disable-line
 
 // createElement
-// const App = defineComponent({
-//   render() {
-//     return createVNode('div', { id: 'app' }, [
-//       createVNode('img', {
-//         alt: 'Vue logo',
-//         src: img,
-//       }),
-//       createVNode(HelloWorld, {
-//         msg: 'Welcome to Your Vue.js + TypeScript App',
-//         age: 12,
-//       }),
-//     ])
-//   },
-// })
+const App = defineComponent({
+  setup() {
+    const state = reactive({
+      name: 'jokcy',
+    })
+
+    const numberRef = ref(1)
+
+    setInterval(() => {
+      state.name += '1'
+      numberRef.value += 1
+    }, 1000)
+
+    // const number = numberRef.value
+
+    return () => {
+      const number = numberRef.value
+      return h('div', { id: 'app' }, [
+        h('img', {
+          alt: 'Vue logo',
+          src: img,
+        }),
+        h('p', state.name + number),
+      ])
+    }
+  },
+})
 
 createApp(App).mount('#app')
