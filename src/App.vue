15201c3a907117c9be50f215b6d4284cc0514181
Jokcy <jokcylou@hotmail.com>
2020-08-30 11:26:27 +0800

    finish 2-6

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :age="12" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>{{ name }} : {{ name2 }} - {{ ref2 }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import {
+  defineComponent,
+  PropType,
+  reactive,
+  ref,
+  computed,
+  watchEffect,
+} from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
@@ -14,9 +22,38 @@ export default defineComponent({
   components: {
     HelloWorld,
   },
-  data() {
+  setup(props, { slots, attrs, emit }) {
+    // let name = 'jokcy'
+    // const state = reactive({
+    //   name: 'jokcy',
+    // })
+
+    // { value: xxx }
+    const nameRef = ref('jokcy')
+    const ref2 = ref(123)
+
+    setInterval(() => {
+      // state.name += '1'
+      nameRef.value += '1'
+    }, 5000)
+
+    setInterval(() => {
+      // state.name += '1'
+      ref2.value += 10
+    }, 1000)
+
+    const computedNameRef = computed(() => {
+      return nameRef.value + '2'
+    })
+
+    watchEffect(() => {
+      console.log(nameRef.value)
+    })
+
     return {
-      name: 'jokcy',
+      name: nameRef,
+      name2: computedNameRef,
+      ref2,
     }
   },
 })
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import { createApp, defineComponent, h } from 'vue'
+import { createApp, defineComponent, createVNode } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-// import App from './App.vue'
+import App from './App.vue'
 
 const img = require('./assets/logo.png') // eslint-disable-line
 
 // createElement
-const App = defineComponent({
-  render() {
-    return h('div', { id: 'app' }, [
-      h('img', {
-        alt: 'Vue logo',
-        src: img,
-      }),
-      h(HelloWorld, {
-        msg: 'Welcome to Your Vue.js + TypeScript App',
-        age: 12,
-      }),
-    ])
-  },
-})
+// const App = defineComponent({
+//   render() {
+//     return createVNode('div', { id: 'app' }, [
+//       createVNode('img', {
+//         alt: 'Vue logo',
+//         src: img,
+//       }),
+//       createVNode(HelloWorld, {
+//         msg: 'Welcome to Your Vue.js + TypeScript App',
+//         age: 12,
+//       }),
+//     ])
+//   },
+// })
 
 createApp(App).mount('#app')
