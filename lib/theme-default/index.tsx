369ad461e13ba03538b1e3427c8375997f90dd4b
Jokcy <jokcylou@hotmail.com>
2020-10-04 15:50:50 +0800

    finish 7-2

---
+++ lib/theme-default/index.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ThemeDefault',
+  setup() {
+    return () => <div>Helle Theme</div>
+  },
+})
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,17 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const CircularDependencyPlugin = require('circular-dependency-plugin')
 
+const isLib = process.env.TYPE === 'lib'
+
 module.exports = {
   configureWebpack(config) {
     // console.log(config.plugins)
   },
   chainWebpack(config) {
-    config.plugin('monaco').use(new MonacoWebpackPlugin())
+    if (!isLib) {
+      config.plugin('monaco').use(new MonacoWebpackPlugin())
+    }
     config.plugin('circular').use(new CircularDependencyPlugin())
   },
+  pwa: {},
 }
