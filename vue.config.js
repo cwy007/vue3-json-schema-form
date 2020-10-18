9b9d4692470fc63d80f2dbcd98604f05e4c3a551
Jokcy <jokcylou@hotmail.com>
2020-10-18 16:51:04 +0800

    finish 11-4

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const CircularDependencyPlugin = require('circular-dependency-plugin')
 
+// https://codecov.io/gh/Jokcy/vjsf-imooc/branch/11-4
+
 const isLib = process.env.TYPE === 'lib'
 
 module.exports = {
